c5e2ec9341fe763e8206c5dda1238b49ad1c26ae
kaminora <kaminoura9207@gmail.com>
2018-02-10 14:00:56 +0900

    :recycle: Put out TweetForm component

--- react-tweet/src/components/tweet/index.js
+++ react-tweet/src/components/tweet/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
 export default class Tweet extends React.PureComponent {
+  componentWillMount() {
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <TweetForm />
+        </div>
+      </div>
+    )
+  }
+}
+
+class TweetForm extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.state = {textValue: ''}
+    this.state = {
+      textValue: '',
+    }
   }
 
   componentWillMount() {
     this.changeText = this.changeText.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   changeText(e) {
@@ -20,14 +38,10 @@ export default class Tweet extends React.PureComponent {
 
   render() {
     return (
-      <div>
-        <div className="TweetForm">
-          <form action="javascript:void(0)" onSubmit={this.handleSubmit}>
-            <input type="text" value={this.state.textValue} onChange={this.changeText} />
-            <button type="submit">送信</button>
-          </form>
-        </div>
-      </div>
+      <form action="javascript:void(0)" onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.textValue} onChange={this.changeText} />
+        <button type="submit">送信</button>
+      </form>
     )
   }
 }
