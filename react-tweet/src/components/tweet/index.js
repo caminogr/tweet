169e88160802abdad751aa47f81c2e8a643d15f0
kaminora <kaminoura9207@gmail.com>
2018-02-10 13:16:02 +0900

    :+1: Add submit button

--- react-tweet/src/components/tweet/index.js
+++ react-tweet/src/components/tweet/index.js
@@ -6,15 +6,26 @@ export default class Tweet extends React.PureComponent {
     this.state = {textValue: ''}
   }
 
+  componentWillMount() {
+    this.changeText = this.changeText.bind(this);
+  }
+
   changeText(e) {
     this.setState({textValue: e.target.value});
+  }
+
+  handleSubmit() {
+    console.log("submit")
   }
 
   render() {
     return (
       <div>
         <div className="TweetForm">
-          <input type="text" value={this.state.textValue} onChange={this.changeText.bind(this)} />
+          <form action="javascript:void(0)" onSubmit={this.handleSubmit}>
+            <input type="text" value={this.state.textValue} onChange={this.changeText} />
+            <button type="submit">送信</button>
+          </form>
         </div>
       </div>
     )
