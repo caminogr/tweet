526def3a0f429289e97234325ceb3203ac9483f8
kaminora <kaminoura9207@gmail.com>
2018-02-11 00:57:01 +0900

    :+1: Connect store and tweetList component

--- react-redux-tweet/src/components/tweet/tweetList.js
+++ react-redux-tweet/src/components/tweet/tweetList.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-export default class TweetList extends React.PureComponent {
+import * as TweetActions from '../../actions/tweet'
+
+class TweetList extends React.PureComponent {
   render() {
     return (
       <div>TweetList</div>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    tweets: state.tweets,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    tweetActions: bindActionCreators(TweetActions, dispatch),
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TweetList);
