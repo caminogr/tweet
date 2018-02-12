071daefd12a9ae79f0f5caff2dd7d0be5ed83469
kaminora <kaminoura9207@gmail.com>
2018-02-13 03:40:00 +0900

    :sparkles: Add feature to post message

--- react-flux-tweet/src/components/tweet/tweetForm.js
+++ react-flux-tweet/src/components/tweet/tweetForm.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
-export default (props) => {
-  return (
-    <div>
-      form 
-    </div>
-  )
+import {Actions} from '../../actions/tweet';
+
+export default class TweetForm extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textValue: '',
+    }
+  }
+
+  componentWillMount() {
+    this.changeText = this.changeText.bind(this);
+    this.handleSendTweet = this.handleSendTweet.bind(this);
+  }
+
+  handleSendTweet(e) {
+    e.preventDefault();
+    Actions.createAction(this.state.textValue)
+    this.setState({textValue: ''})
+  }
+
+  changeText(e) {
+    this.setState({textValue: e.target.value});
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSendTweet}>
+        <input type="text" value={this.state.textValue} onChange={this.changeText} />
+        <button type="submit">送信</button>
+      </form>
+    )
+  }
 }
--- react-flux-tweet/src/components/tweet/tweetList.js
+++ react-flux-tweet/src/components/tweet/tweetList.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
-export default (props) => {
-  return (
-    <div>
-      list
-    </div>
-  )
+import TweetStore from '../../stores/tweetStore';
+
+export default class TweetForm extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tweets: TweetStore.getAll(),
+    }
+
+    TweetStore.on("CHANGE", () => {
+        this._onChange();
+    });
+  }
+
+  _onChange() {
+    this.setState({ tweets: TweetStore.getAll() });
+  }
+
+  render() {
+    const tweets = this.state.tweets
+
+    return (
+      <ul>
+        {tweets.length ? (
+          tweets.map((tweet) => 
+            <li key={tweet.id}>
+              {tweet.content}
+            </li>
+          )
+        ) : (
+        <div>
+          投稿なし
+        </div>
+        )}
+      </ul>
+    )
+  }
 }
